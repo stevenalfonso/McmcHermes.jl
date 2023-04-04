724642e0dac705e8c88feb9ddf13a15a23807817
stevenalfonso <stevenalfonso0@gmail.com>
2023-04-03 21:50:22 -0500

    doc

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 =deploydocs(
-    repo = "https://github.com/stevenalfonso/McmcHermes.jl.git"
+    repo = "github.com/stevenalfonso/McmcHermes.jl.git"
 )=#
 
 
