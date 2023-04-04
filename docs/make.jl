c38667dbb4193435d4c666c888cc8fcb7d6fe063
stevenalfonso <stevenalfonso0@gmail.com>
2023-04-03 21:41:30 -0500

    modif workflows

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
+=deploydocs(
     repo = "https://github.com/stevenalfonso/McmcHermes.jl.git"
 )=#
 
