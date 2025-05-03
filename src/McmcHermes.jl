324a2a95fa5178e32fa90b3e51538265f37706d5
Steven Alfonso <stevenalfonso0@gmail.com>
2025-05-03 11:09:14 -0500

    test changes

--- src/McmcHermes.jl
+++ src/McmcHermes.jl
@@ -1,19 +1,9 @@
 module McmcHermes
 
 using Distributions
+using ProgressMeter
 
 export one_mcmc, run_mcmc, get_flat_chain, gelman_rubin_diagnostic, sampler
-
-
-
-"""
-
-one_mcmc(log_prob, data, parameters, n_iter, a)
-
-Returns one chain with dimension of (n_iter, 1, n_dim)
-
-"""
-
 
 
 function one_mcmc(log_prob::Function, 
@@ -21,6 +11,13 @@ function one_mcmc(log_prob::Function,
     parameters::Vector, 
     n_iter::Int64, 
     a::Float64)
+
+    """
+
+    one_mcmc(log_prob, data, parameters, n_iter, a)
+    Returns one chain with dimension of (n_iter, 1, n_dim)
+
+    """
 
     x = ones(( n_iter, length(parameters) ))
     x[1,:] = [parameters][1]
@@ -54,16 +51,6 @@ end
 
 
 
-"""
-
-run_mcmc(log_prob, data, seed, n_iter, n_walkers, n_dim, a)
-
-Returns chains with dimension of (n_iter, n_walkers, n_dim)
-
-"""
-
-
-
 function run_mcmc(log_prob::Function, 
     data::Vector, 
     seed::Matrix, 
@@ -72,29 +59,36 @@ function run_mcmc(log_prob::Function,
     n_dim::Int64; 
     a::Float64=0.1)
 
-    println("Running chains...")
+    """
+
+    run_mcmc(log_prob, data, seed, n_iter, n_walkers, n_dim, a)
+    Returns chains with dimension of (n_iter, n_walkers, n_dim)
+
+    """
 
     mcmc_chains = ones(( n_iter, n_dim, n_walkers ))
 
-    start_time = time_ns()
-
-    Threads.@threads for walkers in 1:n_walkers
+    @showprogress "Running chains..." for walkers in 1:n_walkers
         one_chain = one_mcmc(log_prob, data, seed[walkers,:], n_iter, a)
         mcmc_chains[:,:,walkers] = one_chain
     end
 
-    elapsed_time = (time_ns() - start_time) / 1_000_000_000  # Convert to seconds
+    # start_time = time_ns()
 
-    if elapsed_time < 60
-        println("Time elapsed running mcmc: $(elapsed_time) seconds.")
+    # Threads.@threads for walkers in 1:n_walkers
+    #     one_chain = one_mcmc(log_prob, data, seed[walkers,:], n_iter, a)
+    #     mcmc_chains[:,:,walkers] = one_chain
+    # end
 
-    elseif (elapsed_time >= 60) && (elapsed_time < 3600)
-        println("Time elapsed running mcmc: $(elapsed_time / 60) minutes.")
+    # elapsed_time = (time_ns() - start_time) / 1_000_000_000  # Convert to seconds
 
-    else
-        println("Time elapsed running mcmc: $(elapsed_time / 3600) hours.")
-
-    end
+    # if elapsed_time < 60
+    #     println("Time elapsed running mcmc: $(elapsed_time) seconds.")
+    # elseif (elapsed_time >= 60) && (elapsed_time < 3600)
+    #     println("Time elapsed running mcmc: $(elapsed_time / 60) minutes.")
+    # else
+    #     println("Time elapsed running mcmc: $(elapsed_time / 3600) hours.")
+    # end
 
     chains = permutedims(mcmc_chains, [1, 3, 2])
 
@@ -103,18 +97,16 @@ function run_mcmc(log_prob::Function,
 end
 
 
+function get_flat_chain(array::Array; 
+    burn_in::Int=1, 
+    thin::Int=1)
 
-"""
+    """
 
-get_flat_chain(array, burn_in, thin) 
+    get_flat_chain(array, burn_in, thin) 
+    Returns the stored chain of MCMC samples.
 
-Returns the stored chain of MCMC samples.
-
-"""
-
-
-
-function get_flat_chain(array::Array; burn_in::Int=1, thin::Int=1)
+    """
     
     l = ones(( size([i for i in 1:size(array)[1] if i%thin==0])[1], size(array)[2], size(array)[3] ))
     ind = 0
@@ -133,17 +125,15 @@ function get_flat_chain(array::Array; burn_in::Int=1, thin::Int=1)
 end
 
 
-"""
-
-gelman_rubin_diagnostic(chains)
-
-Get the Gelman Rubin convergence diagnostic of the chains.
-Returns the Gelman-Rubin number.
-
-"""
-
-
 function gelman_rubin_diagnostic(chains)
+
+    """
+
+    gelman_rubin_diagnostic(chains)
+    Get the Gelman Rubin convergence diagnostic of the chains.
+    Returns the Gelman-Rubin number.
+
+    """
 
     s_j = var(chains, dims=2) # within chain variance
     W = mean(s_j)
@@ -162,21 +152,17 @@ end
 
 
 
-"""
-sampler(pdf, n_samples, intervals, params)
-
-Get n_samples samples from a 1D Probability Density Distribution
-given some parameters. The interval works for the range of samples.
-
-"""
-
-
-
 function sampler(pdf::Function, 
     n_samples::Number, 
     interval::Vector, 
     params::Vector)
 
+    """
+    sampler(pdf, n_samples, intervals, params)
+    Get n_samples samples from a 1D Probability Density Distribution
+    given some parameters. The interval works for the range of samples.
+
+    """
 
     #states = []
     states = ones(n_samples)
